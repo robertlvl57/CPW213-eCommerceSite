eb8d0dcb9b688e82847307298849fa659e8d00a0
robert lowry <lowryrob2001@gmail.com>
2019-07-23 09:50:49 -0700

    Created starting VideoGame class

---
+++ eCommerce/Models/VideoGame.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.Models
+{
+    /// <summary>
+    /// Represents a single video game
+    /// </summary>
+    public class VideoGame
+    {
+        /// <summary>
+        /// Unique ID number for the game
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The game title
+        /// </summary>
+        [Required]
+        [StringLength(90)]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Official ESRB rating
+        /// </summary>
+        public string Rating { get; set; }
+
+        /// <summary>
+        /// The game description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Retail price for the game
+        /// </summary>
+        [Range(0.01, 999.99)]
+        [DataType(DataType.Currency)]
+        // Required by default because double is a value type
+        public double Price { get; set; }
+    }
+}
