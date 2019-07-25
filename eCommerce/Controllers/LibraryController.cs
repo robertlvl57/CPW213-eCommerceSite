0878a3ff6fe2d79fae8117c43c8db2f7198e70e8
robert lowry <lowryrob2001@gmail.com>
2019-07-25 09:45:17 -0700

    Added Game library page and game add page

---
+++ eCommerce/Controllers/LibraryController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class LibraryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(VideoGame game)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add to database
+                return RedirectToAction("Index");
+            }
+
+            // Return view with model including error messages
+            return View(game);
+        }
+    }
+}
