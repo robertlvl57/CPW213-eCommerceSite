67602fd0f58eaeb02a8b00419544da9de5966a0c
robert lowry <lowryrob2001@gmail.com>
2019-08-08 10:22:17 -0700

    Added member to the database

--- eCommerce/Data/GameContext.cs
+++ eCommerce/Data/GameContext.cs
@@ -19,5 +19,6 @@ namespace eCommerce.Data
 
         // Add a DbSet<T> for each entity you wnat to keep track of in the database
         public DbSet<VideoGame> VideoGames { get; set; }
+        public DbSet<Member> Members { get; set; }
     }
 }
