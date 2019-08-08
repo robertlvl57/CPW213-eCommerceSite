3f94b9f0ebaf19c76e75e01be03f3c2d79acf30f
robert lowry <lowryrob2001@gmail.com>
2019-08-08 10:15:32 -0700

    Added Member class

---
+++ eCommerce/Models/Member.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.Models
+{
+    /// <summary>
+    /// Represents an individual website user
+    /// </summary>
+    public class Member
+    {
+        /// <summary>
+        /// The first and last name of the Member.
+        /// es. J. Doe
+        /// </summary>
+        [StringLength(60)]
+        [Required]
+        [Display(Name = "Full name")]
+        public string FullName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Email address")]
+        [EmailAddress(ErrorMessage = "That doesn't look like an email")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        [RegularExpression(@"^[\d\w]+$",
+            ErrorMessage = "Usernames can only contain A-Z, 0-9, and underscores")]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// The date of birth for the member. Time is ignored.
+        /// </summary>
+        [Display(Name = "Date of birth")]
+        [DataType(DataType.Date)]
+        // Required because DateTime is a structure (it's a value type)
+        public DateTime DateOfBirth { get; set; }
+    }
+}
