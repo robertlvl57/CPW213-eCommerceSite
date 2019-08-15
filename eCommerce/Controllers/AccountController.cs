962a339e0354fac117e8fcd5d097aee0a4f353d1
robert lowry <lowryrob2001@gmail.com>
2019-08-15 10:24:41 -0700

    Configured session and created session during login

--- eCommerce/Controllers/AccountController.cs
+++ eCommerce/Controllers/AccountController.cs
@@ -4,17 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using eCommerce.Data;
 using eCommerce.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerce.Controllers
 {
     public class AccountController : Controller
     {
-        private GameContext _context;
+        /// <summary>
+        /// Provides access to session data for the current user
+        /// </summary>
+        private readonly IHttpContextAccessor _httpAccessor;
+        private readonly GameContext _context;
 
-        public AccountController(GameContext context)
+        public AccountController(GameContext context, IHttpContextAccessor accessor)
         {
             _context = context;
+            _httpAccessor = accessor;
         }
 
         [HttpGet]
@@ -37,19 +43,24 @@ namespace eCommerce.Controllers
             return View(m);
         }
 
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
 
+        [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel model)
         {
             if (ModelState.IsValid)
             {
-                bool isMember = await MemberDb.IsLoginValid(model, _context);
-                if (isMember)
+                Member member = await MemberDb.IsLoginValid(model, _context);
+                if (member != null)
                 {
                     TempData["Message"] = "Logged in successfully";
+                    // Create session for user
+                    _httpAccessor.HttpContext.Session.SetInt32("MemberId", member.MemberId);
+                    _httpAccessor.HttpContext.Session.SetString("Username", member.Username);
                     return RedirectToAction("Index", "Home");
                 }
                 else // Credential invalid
