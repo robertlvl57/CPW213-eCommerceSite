c7b5dfe3ad5e29345e8ab56327fde2edaee5644d
robert lowry <lowryrob2001@gmail.com>
2019-08-22 10:43:45 -0700

    Added shopping cart icon with item count displayed

--- eCommerce/Controllers/CartController.cs
+++ eCommerce/Controllers/CartController.cs
@@ -37,8 +37,13 @@ namespace eCommerce.Controllers
             //};
 
             //_httpAccessor.HttpContext.Response.Cookies.Append("CartCookie", data, options);
+        }
 
-            
+        public IActionResult Checkout()
+        {
+            List<VideoGame> games = CartHelper.GetGames(_httpAccessor);
+
+            return View(games);
         }
     }
 }
